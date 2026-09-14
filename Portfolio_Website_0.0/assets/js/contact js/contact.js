aed8a46a6259e970e27365dac2642e69402db4ae
ketansharma91 <ketansharma.ks.911@gmail.com>
2026-09-14 13:11:07 +0530

    added portfolio site 0.0

---
+++ Portfolio_Website_0.0/assets/js/contact js/contact.js
@@ -0,0 +1,10 @@
+// contact js code
+var messageText = "Mail Sent";
+        if(messageText != ''){
+            Swal.fire({
+                title: "Thank you!",
+                text: messageText,
+                icon: "success"
+            });
+             
+        }
---
+++ Portfolio_Website_0.0/assets/js/script.js
@@ -0,0 +1,52 @@
+// Type js Code
+const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+        return [...document.querySelectorAll(el)]
+    } else {
+        return document.querySelector(el)
+    }
+}
+const typed = select('.typed')
+if (typed) {
+    let typed_strings = typed.getAttribute('data-typed-items')
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+        strings: typed_strings,
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 2000
+    });
+}
+
+//toggle icon navbar
+let menuIcon = document.querySelector('.menu');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('menu');
+    navbar.classList.toggle('active');
+};
+
+// scroll sections active link
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+            
+};
+
